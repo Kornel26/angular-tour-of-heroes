c06eee0e02b84cfc3f9894457314b879e48671a4
Kornél Horváth <khorvath@diligent.com>
2022-07-27 16:46:32 +0200

    test commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'project1';
+  title:string = 'Tour of movies';
+  description:string = "Hát igen ez nagyon jó."
 }
